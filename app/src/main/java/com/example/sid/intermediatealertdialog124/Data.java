1ba53978dfbd4f986a06b12bb013e88b758cbe10
Sid498 <sidpawar619@gmail.com>
2017-08-11 15:25:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sid/intermediatealertdialog124/Data.java
@@ -0,0 +1,36 @@
+package com.example.sid.intermediatealertdialog124;
+
+/**
+ * Created by SID on 8/11/2017.
+ */
+
+public class Data {
+
+    String mName;
+    int mNumber;
+    String mDate;
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public int getmNumber() {
+        return mNumber;
+    }
+
+    public void setmNumber(int mNumber) {
+        this.mNumber = mNumber;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
+    }
+}
